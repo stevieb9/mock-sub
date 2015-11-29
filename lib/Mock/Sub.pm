b960b5a56c94672fb6670d48daa689a51b6364a3
Steve Bertrand <steveb@cpan.org>
2015-11-29 14:21:07 -0700

    Added EXAMPLE section in POD

--- lib/Mock/Sub.pm
+++ lib/Mock/Sub.pm
@@ -72,21 +72,25 @@ Mock::Sub - Mock package, module, object and standard subs, with ability to coll
 
     my $foo = Mock::Sub->mock('Package::foo');
     my $bar = Mock::Sub->mock('Package::bar');
+    my $baz = Mock::Sub->mock('Package::baz');
 
     # wait until the mocked sub is called
 
     Package::foo();
 
-    say $foo->name;         # name of sub that's mocked
-    say $foo->called;       # was the sub called?
-    say $foo->call_count;   # how many times was it called?
+    # then...
 
-    # create an object to reduce typing
+    $foo->name;         # name of sub that's mocked
+    $foo->called;       # was the sub called?
+    $foo->call_count;   # how many times was it called?
+
+    # create a mock object to reduce typing
 
     my $mock = Mock::Sub->new;
     
     my $foo = $mock->('Package::foo');
     my $bar = $mock->('Package::bar');
+    my $baz = $mock->('Package::baz');
 
     # have the mocked sub return something when it's called
 
@@ -97,7 +101,62 @@ Mock::Sub - Mock package, module, object and standard subs, with ability to coll
     $foo = $mock->('Package::foo', side_effect => sub { die "eval catch"; });
     
 
+=head1 EXAMPLE
+
+Here's a full example to get further coverage where it's difficult if not
+impossible to test certain areas of your code (eg: you have if/else statements,
+but they don't do anything but call other subs. You don't want to test the
+subs that are called, nor do you want to add statements to your code).
+
+Note that if the end subroutine you're testing is NOT Object Oriented (and
+you're importing them into your module that you're testing), you have to mock
+them as part of your own namespace (ie. instead of Other::first, you'd mock
+MyModule::first).
+
+   # module you're testing:
+
+    package MyPackage;
+    
+    use Other;
+    use Exporter qw(import);
+    @EXPORT_OK = qw(test);
+   
+    my $other = Other->new;
+
+    sub test {
+        my $arg = shift;
+        
+        if ($arg == 1){
+            # how do you test this... there's no return etc.
+            $other->first();        
+        }
+        if ($arg == 2){
+            $other->second();
+        }
+    }
+
+    # your test file
+
+    use MyPackage qw(test);
+    use Mock::Sub;
+    use Test::More tests => 2;
+
+    my $mock = Mock::Sub->new;
+
+    my $first = $mock->mock('Other::first');
+    my $second = $mock->mock('Other::second');
+
+    # coverage for first if() in MyPackage::test
+    test(1);
+    is ($first->called, 1, "first if() covered");
+
+    # coverage for second if()
+    test(2);
+    is ($second->called, 1, "first if() covered");
+
+
 =head1 METHODS
+
 
 
 =head1 AUTHOR
